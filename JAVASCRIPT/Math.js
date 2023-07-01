50c816974c8d5a79f146d456e666330482096981
wmvj <warleyjunior05@gmail.com>
2023-07-01 20:58:36 -0300

    JavaScript - Fundamentos

---
+++ JAVASCRIPT/Math.js
@@ -0,0 +1,12 @@
+let num1 = 9.7890
+// let num2 = Math.ceil(num1)
+// let num2 = Math.floor(num1)
+// let num2 = Math.round(num1)
+// console.log(Math.max(1,2,3,4,5))
+// console.log(Math.min(num1,2,3,4,5))
+const aleatorio = Math.round(Math.random() * (10-5) + 5)
+console.log(aleatorio)
+
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+console.log(area.toFixed(2))
---
+++ JAVASCRIPT/tipagem.js
@@ -0,0 +1,10 @@
+let qualquer = 'legal'
+qualquer = 23
+
+console.log(qualquer)
+console.log(typeof qualquer)
+
+// evitar nome genéricos e siglas
+let valor = ""
+let pqp = false
+let numero = 1
